b0798767eea5e3db373fe53600496f3045a2735b
FahzAtmin <216216596+FahzAtmin@users.noreply.github.com>
2025-10-05 14:04:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   tokens: "8317840688:AAEiDH7oYzJv4wlHUpPeA2Pf9OD9c_jvv2Y",
   owner: "8459065155",
   port: "7000",
-  ipvps: "http://rulstr.storedigital.web.id"
+  ipvps: "http://rulstr.storedigital.web.id:7000"
 };
